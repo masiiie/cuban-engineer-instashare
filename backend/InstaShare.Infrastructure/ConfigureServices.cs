d1061feea7be246c6f19ccb165a04c47dedff9be
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-14 09:10:56 -0400

    defining addInfrastructure functionality

---
+++ backend/InstaShare.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using InstaShare.Infrastructure.Persistence;
+
+namespace InstaShare.Infrastructure;
+
+public static class ConfigureService
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(
+            options => {
+                options.UseNpgsql(
+                        configuration.GetConnectionString("postgresConnection"),
+                        npgsqlOptions =>
+                        {
+                            npgsqlOptions.EnableRetryOnFailure();
+                        });
+
+                options.EnableDetailedErrors();
+                options.EnableSensitiveDataLogging();                
+            }
+        );
+    
+        return services;
+    }
+}
