c49ec18d0682e6ea61586c8c665eaa91001ca6d1
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-15 07:28:11 -0400

    adding instashare files to dbcontext

--- backend/InstaShare.Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/InstaShare.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using InstaShare.Domain.Entities.Files;
 
 namespace InstaShare.Infrastructure.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+        public DbSet<InstaShareFile> Files => Set<InstaShareFile>();
     }
 }
