adb29a778062a62589aa846b012d7bdede1a542d
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-14 21:27:09 -0400

    renaming entity

--- backend/InstaShare.Domain/Entities/File/InstaShareFile.cs
+++ backend/InstaShare.Domain/Entities/File/InstaShareFile.cs
@@ -2,7 +2,7 @@ using InstaShare.Domain.Entities.Common;
 
 namespace InstaShare.Domain.Entities.Files
 {
-    public class File : AuditableBase
+    public class InstaShareFile : AuditableBase
     {
         public string Name {get;set;}
         public FileStatus Status {get;set;}
