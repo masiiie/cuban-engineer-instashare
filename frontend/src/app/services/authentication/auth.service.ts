7996841bca0af532eab35eec78be8195416a536b
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-16 13:26:33 -0400

    auth service baseline

--- frontend/src/app/services/authentication/auth.service.ts
+++ frontend/src/app/services/authentication/auth.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+
+interface AuthResponse {
+  token: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = environment.instaShareApiUrl;
+  private loggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(username: string, password:string) {
+    return this.http.post<AuthResponse>(
+      `${this.apiUrl}/login`,
+      {username, password})
+      .subscribe(response => {
+        localStorage.setItem('token', response.token);
+        this.loggedIn.next(true);
+        this.router.navigate(['/']);
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.loggedIn.next(false);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn() {
+    return this.loggedIn.asObservable();
+  }
 }
