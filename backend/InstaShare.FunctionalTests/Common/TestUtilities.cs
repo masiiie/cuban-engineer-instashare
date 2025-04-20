6f89547eff4a243b5a44a0ae25479c181b9ce49f
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-20 19:25:42 -0400

    adding utilities to be used for all tests

---
+++ backend/InstaShare.FunctionalTests/Common/TestUtilities.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using InstaShare.Infrastructure.Persistence;
+using InstaShare.Domain.Entities.Files;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InstaShare.FunctionalTests.Common;
+
+public static class TestUtilities
+{
+}
