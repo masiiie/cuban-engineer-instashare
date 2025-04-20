5ba6c7892be1c142fde1a32188503c27782330be
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-20 19:38:46 -0400

    enabling application dbcontext to be used in all test classes

--- backend/InstaShare.FunctionalTests/Common/BaseFunctionalTest.cs
+++ backend/InstaShare.FunctionalTests/Common/BaseFunctionalTest.cs
@@ -1,14 +1,20 @@
 ﻿using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using InstaShare.Infrastructure.Persistence;
+using System.Net.Http;
 
-namespace Worsite.FunctionalTests.Common;
+namespace InstaShare.FunctionalTests.Common;
 
 [Collection("PostgresTestDatabase")]
 public class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
 {
+    protected readonly HttpClient httpclient;
+    protected readonly ApplicationDbContext _dbContext;
+
     public BaseFunctionalTest(FunctionalTestWebAppFactory factory)
     {        
         httpclient = factory.CreateClient();
+        var scope = factory.Services.CreateScope();
+        _dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     }
-
-    protected HttpClient httpclient { get; init; }
 }
