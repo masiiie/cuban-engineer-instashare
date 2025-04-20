d12d18db65e7edac7eba7e419c40ab159fc99dee
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-20 19:40:36 -0400

    adding getting existing file functionality to utility tests

--- backend/InstaShare.FunctionalTests/Common/TestUtilities.cs
+++ backend/InstaShare.FunctionalTests/Common/TestUtilities.cs
@@ -4,9 +4,28 @@ using InstaShare.Infrastructure.Persistence;
 using InstaShare.Domain.Entities.Files;
 using System.Threading;
 using System.Threading.Tasks;
+using InstaShare.Domain.Entities.Files;
+
 
 namespace InstaShare.FunctionalTests.Common;
 
 public static class TestUtilities
 {
+    public static async Task<int> GetExistingFileIdAsync(ApplicationDbContext dbContext)
+    {        
+        var file = await dbContext.Files.FirstOrDefaultAsync();
+        
+        if (file == null)
+        {
+            // If no files exist in the database, create one
+            var newFile = new InstaShareFile("Test File", 1024, "text/plain");
+            dbContext.Files.Add(newFile);
+            using var cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return (int)newFile.Id;
+        }
+        
+        return (int)file.Id;
+    }
 }
