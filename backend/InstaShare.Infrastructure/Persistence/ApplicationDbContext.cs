939b90a624891352d20bf361b60dd8ffc6eb6600
Masiel Villalba <villalbamasiel@gmail.com>
2025-04-21 22:56:22 -0400

    keeping lastmodified and created fields updated inside the savechangesasync method

--- backend/InstaShare.Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/InstaShare.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+
+using InstaShare.Domain.Entities.Common;
 using InstaShare.Domain.Entities.Files;
 
 namespace InstaShare.Infrastructure.Persistence
@@ -10,5 +13,25 @@ namespace InstaShare.Infrastructure.Persistence
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     
         public DbSet<InstaShareFile> Files => Set<InstaShareFile>();
+
+        public sealed override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is AuditableBase && (
+                e.State == EntityState.Added
+                || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((AuditableBase)entityEntry.Entity).LastModified = DateTime.UtcNow;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                ((AuditableBase)entityEntry.Entity).Created = DateTime.UtcNow;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
